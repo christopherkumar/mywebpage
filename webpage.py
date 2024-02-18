9faf9956383b488740cce332e9ada40ad60a4dde
christopherkumar <160341137+christopherkumar@users.noreply.github.com>
2024-02-19 00:15:16 +1000

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -6,16 +6,19 @@ from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
+
 def load_lottieurl(url):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
 
+
 # Use local CSS
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 
 local_css("style/style.css")
 
